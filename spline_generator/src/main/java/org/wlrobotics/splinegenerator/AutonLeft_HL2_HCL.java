a705e673b06b27d154d12a18ae72b59b2c732290
WhitmoreLakeTroBots <wlhsroboticsteam@gmail.com>
2019-01-18 17:15:56 -0500

    HAF more splines

--- spline_generator/src/main/java/org/wlrobotics/splinegenerator/AutonLeft_HL2_HCL.java
+++ spline_generator/src/main/java/org/wlrobotics/splinegenerator/AutonLeft_HL2_HCL.java
@@ -4,34 +4,34 @@ import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.Trajectory;
 import jaci.pathfinder.Waypoint;
 
-class AutonLeft_HL3_HCL extends BaseAutonGenerator {
+class AutonLeft_HL2_HCL extends BaseAutonGenerator {
 
-  AutonLeft_HL3_HCL (Trajectory.Config conf) {
+  AutonLeft_HL2_HCL (Trajectory.Config conf) {
     this.config = conf;
     baseFileName = this.getClass().getSimpleName();
-    gnuPlotString = "plot '%s' using 2:3 w lp lt rgb 'steelblue'";
+    gnuPlotString = "plot '%s' using 2:3 w lp lt rgb 'gray'";
   }
 
-  private Trajectory gen_Auton_HL3_HCL_01() {
+  private Trajectory gen_Auton_HL2_HCL_01() {
 		Waypoint[] points = new Waypoint[] {
       new Waypoint(1.50, 5.00, Pathfinder.d2r(0)),
       new Waypoint(5.00, 6.00, Pathfinder.d2r(25)),
-      new Waypoint(7.75, 4.60, Pathfinder.d2r(90))
+      new Waypoint(7.20, 4.60, Pathfinder.d2r(90))
 		};
 		return (Pathfinder.generate(points, config));
     }
 
-    private Trajectory gen_Auton_HL3_HCL_02(){
+    private Trajectory gen_Auton_HL2_HCL_02(){
       Waypoint[] points = new Waypoint[] {
-        new Waypoint(7.75, 4.60, Pathfinder.d2r(90)),
-        new Waypoint(5.00, 6.70, Pathfinder.d2r(160)),
+        new Waypoint(7.20, 4.60, Pathfinder.d2r(90)),
+        new Waypoint(4.75, 6.70, Pathfinder.d2r(160)),
         new Waypoint(0.45, 7.37, Pathfinder.d2r(-180)),
         new Waypoint(0.00, 7.37, Pathfinder.d2r(-180))
       };
       return (Pathfinder.generate(points, config));
     }
 
-    private Trajectory gen_Auton_HL3_HCL_03(){
+    private Trajectory gen_Auton_HL2_HCL_03(){
       Waypoint[] points = new Waypoint[] {
         new Waypoint(0.00, 7.37, Pathfinder.d2r(0)),
         new Waypoint(0.45, 7.37, Pathfinder.d2r(0)),
@@ -41,9 +41,9 @@ class AutonLeft_HL3_HCL extends BaseAutonGenerator {
     }
 
   void generate () {
-    segments.add (gen_Auton_HL3_HCL_01());
-    segments.add (gen_Auton_HL3_HCL_02());
-    segments.add (gen_Auton_HL3_HCL_03());
+    segments.add (gen_Auton_HL2_HCL_01());
+    segments.add (gen_Auton_HL2_HCL_02());
+    segments.add (gen_Auton_HL2_HCL_03());
     write();
   }
 }
--- spline_generator/src/main/java/org/wlrobotics/splinegenerator/TroBotSplineGenerator.java
+++ spline_generator/src/main/java/org/wlrobotics/splinegenerator/TroBotSplineGenerator.java
@@ -14,27 +14,31 @@ public class TroBotSplineGenerator extends Object {
 				Trajectory.Config.SAMPLES_HIGH, deltaTime, maxVel, maxAccel, maxJerk);
 
 		AutonCenter_HCL_CL3 HCL_CL3 = new AutonCenter_HCL_CL3(config);
-		HCL_CL3.generate();
+		//HCL_CL3.generate();
 		HCL_CL3 = null;
 
 		AutonCenter_HCR_CR3 HCR_CR3 = new AutonCenter_HCR_CR3(config);
-		HCR_CR3.generate();
+		//HCR_CR3.generate();
 		HCR_CR3 = null;
 
 		AutonRight_HR1_HCR HR1_HCR = new AutonRight_HR1_HCR(config);
-		HR1_HCR.generate();
+		//HR1_HCR.generate();
 		HR1_HCR = null;
 
 		AutonLeft_HL1_HCL HL1_HCL = new AutonLeft_HL1_HCL(config);
-		HL1_HCL.generate();
+		//HL1_HCL.generate();
 		HL1_HCL = null;
 
 		AutonCenter_HCR_CR2 HCR_CR2 = new AutonCenter_HCR_CR2(config);
 		HCR_CR2.generate();
 		HCR_CR2 = null;
 
+		AutonCenter_HCL_CR2 HCL_CR2 = new AutonCenter_HCL_CR2(config);
+		HCL_CR2.generate();
+		HCL_CR2 = null;
+
 		AutonRight_HR2_HCR HR2_HCR = new AutonRight_HR2_HCR(config);
-		HR2_HCR.generate();
+		//HR2_HCR.generate();
 		HR2_HCR = null;
 
 
@@ -43,12 +47,16 @@ public class TroBotSplineGenerator extends Object {
 		test = null;
 
 		AutonRight_HR3_HCR HR3_HCR = new AutonRight_HR3_HCR(config);
-		HR3_HCR.generate();
+		//HR3_HCR.generate();
 		HR3_HCR = null;
 
 		AutonLeft_HL3_HCL HL3_HLR = new AutonLeft_HL3_HCL(config);
-		HL3_HLR.generate();
+		//HL3_HLR.generate();
 		HL3_HLR = null;
+
+		AutonLeft_HL2_HCL HL2_HLR = new AutonLeft_HL2_HCL(config);
+		//HL2_HLR.generate();
+		HL2_HLR = null;
 
 		System.out.println("TroBot Splines Generation: Complete");
 	}
